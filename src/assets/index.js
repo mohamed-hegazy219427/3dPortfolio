ba99ae05ca37c260f63c7a12f2bae66f0328d099
MohamedHegazy2020 <mohamedhegazy219427@gmail.com>
2024-07-09 01:58:07 +0300

    ...

--- src/assets/index.js
+++ src/assets/index.js
@@ -7,7 +7,7 @@ import github from "./github.png";
 import netlify from "./netlify-icon-seeklogo.svg";
 import menu from "./menu.svg";
 import close from "./close.svg";
-import Ifuture from "./company/Ifuture.png";
+import Ifuture from "./company/ifuture.png";
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
 import figma from "./tech/figma.png";
